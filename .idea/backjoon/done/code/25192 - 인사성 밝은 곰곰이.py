fca7afa389ca3ebb617c3b8d3a2b4b88ae378281
sejin <rlatpwlsdlek@gmail.com>
2023-07-24 00:14:04 +0900

    Baekjoon 25192 solved

---
+++ .idea/backjoon/done/code/25192 - 인사성 밝은 곰곰이.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('../testCase/25192.txt', 'r')
+input = sys.stdin.readline
+
+entered = set()
+cnt = 0
+for _ in range(int(input())):
+    s = input().strip()
+    # 엔터가 존재하면 목록 초기화
+    if s == "ENTER":
+        entered.clear()
+        continue
+
+    # 이미 인사를 했다면 넘김
+    if s in entered:
+        continue
+    # 새 채팅이므로 목록에 추가하고 인사 횟수 +1
+    else:
+        cnt += 1
+        entered.add(s)
+print(cnt)
