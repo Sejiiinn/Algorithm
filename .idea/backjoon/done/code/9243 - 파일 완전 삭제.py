f65c64b83258ef9b416a8ebdafc1f92eaa017442
sejin <rlatpwlsdlek@gmail.com>
2023-10-15 23:39:18 +0900

    Baekjoon 9243 solved

---
+++ .idea/backjoon/done/code/9243 - 파일 완전 삭제.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('../testCase/9243.txt', 'r')
+input = sys.stdin.readline
+
+n = int(input())
+a = input().rstrip()
+b = input().rstrip()
+if n % 2 == 0:
+    print("Deletion succeeded" if a == b else "Deletion failed")
+else:
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            print("Deletion failed")
+            break
+    else:
+        print("Deletion succeeded")
