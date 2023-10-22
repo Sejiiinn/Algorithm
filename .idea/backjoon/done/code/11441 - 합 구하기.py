da30667dca83ccc021badf0b53e6e375572e501f
sejin <rlatpwlsdlek@gmail.com>
2023-10-22 21:25:01 +0900

    Baekjoon 11441 solved

---
+++ .idea/backjoon/done/code/11441 - 합 구하기.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('../testCase/11441.txt', 'r')
+input = sys.stdin.readline
+
+n = int(input())
+lst = [0]
+for i in map(int,input().split()):
+    lst.append(lst[-1] + i)
+for _ in range(int(input())):
+    a, b = map(int,input().split())
+    print(lst[b] - lst[a-1])
